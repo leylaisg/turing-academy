9435cd22d06ee87d0b355cb1c3ff12978e39d553
leylaisg <leyla.isgandarli95@gmail.com>
2024-07-10 20:07:51 +0400

    springboot-amendment1

---
+++ spring-boot-01/src/test/java/az/edu/turing/springboot01/SpringBoot01ApplicationTests.java
@@ -0,0 +1,13 @@
+package az.edu.turing.springboot01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBoot01ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
